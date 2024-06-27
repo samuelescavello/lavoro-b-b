936a4d9ff376ed95cf86b8a4c3d0c9709d16c4c8
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-06-27 18:50:20 +0200

    carusel

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,8 +2,46 @@
     <main>
         <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/209890188.jpg?k=882e748be3114714efa7f001b6ffa97425b1a52a458d3166dea3c1af7c66ac09&o=&hp=1" alt="">
         <div class="container">
-            <h1>zdxfsgvgvsdfgsdfg</h1>
+        <div class="row">
+            <div class="col-12 text-center mt-5 mb-3">
+                <h1>Scopri i fantastici malviti</h1>
+            </div>
+            <div class="col-6">
+                <h2>Cosa visitare...</h2>
+                <h5>...Malvito!</h5>
+                <p>
+                    Malvito sorge sulla cima di un poggio alla destra dell' Esaro ed offre una veduta paesaggistica singolare. Si ritiene sia uno dei pochi centri calabresi di origine Longobarda, sede di Gastaldato.
+                    Dalla documentazione esistente si può stabilire che Malvito ebbe origine tra la fine del VII secolo e gli inizi del successivo, questo grazie al cronista longobardo Paolo Diacono che indica il centro dell' Esaro come una delle più importanti città dell'Italia meridionale.
+                    Durante il pontificato di Benedetto VII (anno 983), la chiesa di Malvito fu elevata a diocesi fino a quando, nel 1160, fu accorpata a quella di San Marco Argentano.
+                    Nel 1057 subì conquista Normanna di Roberto d'Altavilla chiamato il Guiscardo (furbo). Il primo feudatario fu il conte Roberto detto Scalone, terzogenito del guiscardo e della principessa Longobarda Sikelgaita.
+                    I caratteri di fortezza medievali sono ancora oggi visibili: le case sovrapposte in un caratteristico groviglio di viuzze strette che, di tanto in tanto, si aprono su improvvisi larghi dove si trovano antichi palazzi con artistici portali in tufo.
+                    E' da visitare, all'estremità della collina, posto a dominio della vallata, il castello Longobardo-Normanno, le cui origini sono da collocare nel IX - X secolo.
+                    E' composto dal Mastio normanno (torre principale) di forma cilindrica cui si può accedere tramite una scaletta elicoidale. Si può anche entrare dal cortile centrale attraverso una scala chiamata di scala francese, che comunica alla cortina.
+                    Nel cortile vi è anche l'ingresso ad una sala interrata a forma di cripta....
+                </p>
+            </div>
+            <div class="col-6 d-flex justify-content-center">
+                <div id="imageCarousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/209890188.jpg?k=882e748be3114714efa7f001b6ffa97425b1a52a458d3166dea3c1af7c66ac09&o=&hp=1" alt="">
+                        </div>
+                        <!-- Add more carousel items here if needed -->
+                    </div>
+                    <!-- Carousel controls -->
+                    <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
         </div>
+    </div>
+
     </main>
 </template>
 
