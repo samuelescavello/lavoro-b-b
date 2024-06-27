7a09f951a02ee72130b8a053412074a4c9d334e4
samuelescavello <scavellosamuele23@gmail.com>
2024-06-27 18:49:20 +0200

    padding footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -4,18 +4,18 @@
             <h3><em>Bed & Breakfast Antica Casina</em></h3>
             <h4>di Felicetta Campolongo</h4>
         </div>
-        <div class="container p-4 d-flex justify-content-between">
+        <div class="container p-4 d-flex justify-content-around">
             <div>
                 <h4 class="pb-2"><em>contatti</em></h4>
                 <ul>
-                    <li><i class="fa-regular fa-envelope"></i> info@anticacasina.it</li>
-                    <li><i class="fa-solid fa-phone"></i> cell. 347-6463840 / 347-0178257</li>
+                    <li><i class="fa-regular fa-envelope pb-2"></i> info@anticacasina.it</li>
+                    <li><i class="fa-solid fa-phone pb-2"></i> cell. 347-6463840 / 347-0178257</li>
                     <li><i class="fa-solid fa-phone"></i> telefono e fax. 0984-509537</li>
                     <li></li>
                 </ul>
             </div>
             <div>
-                <h4><em>luogo</em></h4>
+                <h4 class="pb-2"><em>luogo</em></h4>
                 <ul>
                     <li><i class="fa-solid fa-location-dot"></i> C/da Trellenze, snc - 87010 Malvito (Cs)</li>
                 </ul>
@@ -33,7 +33,7 @@ export default {
 
 <style lang="scss" scoped>
 Footer {
-    background-color: rgb(33, 33, 33);
+    background-color: #4C4B46;
     color: white;
 
     ul {
