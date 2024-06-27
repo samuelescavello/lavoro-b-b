fbbd9b7ea3937eeb957914aae07b2621a943f95c
samuelescavello <157405002+samuelescavello@users.noreply.github.com>
2024-06-27 16:30:21 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-center">Hello</h1>
+  <i class="fa-solid fa-house"></i>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
