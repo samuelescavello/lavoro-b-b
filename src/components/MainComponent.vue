98f43eec93606ec22e1f9c0ac60f9b1508bcd837
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-06-27 23:33:45 +0200

    more

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,58 +1,146 @@
 <template>
     <main>
-        <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/209890188.jpg?k=882e748be3114714efa7f001b6ffa97425b1a52a458d3166dea3c1af7c66ac09&o=&hp=1" alt="">
+        <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/209890188.jpg?k=882e748be3114714efa7f001b6ffa97425b1a52a458d3166dea3c1af7c66ac09&o=&hp=1"
+            alt="" />
         <div class="container">
-        <div class="row">
-            <div class="col-12 text-center mt-5 mb-3">
-                <h1>Scopri i fantastici malviti</h1>
-            </div>
-            <div class="col-6">
-                <h2>Cosa visitare...</h2>
-                <h5>...Malvito!</h5>
-                <p>
-                    Malvito sorge sulla cima di un poggio alla destra dell' Esaro ed offre una veduta paesaggistica singolare. Si ritiene sia uno dei pochi centri calabresi di origine Longobarda, sede di Gastaldato.
-                    Dalla documentazione esistente si può stabilire che Malvito ebbe origine tra la fine del VII secolo e gli inizi del successivo, questo grazie al cronista longobardo Paolo Diacono che indica il centro dell' Esaro come una delle più importanti città dell'Italia meridionale.
-                    Durante il pontificato di Benedetto VII (anno 983), la chiesa di Malvito fu elevata a diocesi fino a quando, nel 1160, fu accorpata a quella di San Marco Argentano.
-                    Nel 1057 subì conquista Normanna di Roberto d'Altavilla chiamato il Guiscardo (furbo). Il primo feudatario fu il conte Roberto detto Scalone, terzogenito del guiscardo e della principessa Longobarda Sikelgaita.
-                    I caratteri di fortezza medievali sono ancora oggi visibili: le case sovrapposte in un caratteristico groviglio di viuzze strette che, di tanto in tanto, si aprono su improvvisi larghi dove si trovano antichi palazzi con artistici portali in tufo.
-                    E' da visitare, all'estremità della collina, posto a dominio della vallata, il castello Longobardo-Normanno, le cui origini sono da collocare nel IX - X secolo.
-                    E' composto dal Mastio normanno (torre principale) di forma cilindrica cui si può accedere tramite una scaletta elicoidale. Si può anche entrare dal cortile centrale attraverso una scala chiamata di scala francese, che comunica alla cortina.
-                    Nel cortile vi è anche l'ingresso ad una sala interrata a forma di cripta....
-                </p>
-            </div>
-            <div class="col-6 d-flex justify-content-center">
-                <div id="imageCarousel" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/209890188.jpg?k=882e748be3114714efa7f001b6ffa97425b1a52a458d3166dea3c1af7c66ac09&o=&hp=1" alt="">
-                        </div>
-                        <!-- Add more carousel items here if needed -->
+            <div class="row mb-3">
+                <div class="col-12 text-center mt-5 mb-3">
+                    <h1>Scopri i fantastici malviti</h1>
+                </div>
+
+                <div class="row my-5">
+                    <div class="col-6">
+                        <h2>Cosa visitare...</h2>
+                        <h5>...Malvito!</h5>
+                        <p>
+                            Malvito sorge sulla cima di un poggio alla destra dell' Esaro ed
+                            offre una veduta paesaggistica singolare. Si ritiene sia uno dei
+                            pochi centri calabresi di origine Longobarda, sede di Gastaldato.
+                            Dalla documentazione esistente si può stabilire che Malvito ebbe
+                            origine tra la fine del VII secolo e gli inizi del successivo,
+                            questo grazie al cronista longobardo Paolo Diacono che indica il
+                            centro dell' Esaro come una delle più importanti città dell'Italia
+                            meridionale. Durante il pontificato di Benedetto VII (anno 983), la
+                            chiesa di Malvito fu elevata a diocesi fino a quando, nel 1160, fu
+                            accorpata a quella di San Marco Argentano. Nel 1057 subì conquista
+                            Normanna di Roberto d'Altavilla chiamato il Guiscardo (furbo). Il
+                            primo feudatario fu il conte Roberto detto Scalone, terzogenito del
+                            guiscardo e della principessa Longobarda Sikelgaita.
+                        </p>
                     </div>
-                    <!-- Carousel controls -->
-                    <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+
+                    <div class="col-6 ">
+                        <div id="carouselExampleIndicators" class="carousel slide">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                                    class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                                    aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                                    aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
+                                        class="d-block w-100" alt="..." />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
+                                        class="d-block w-100" alt="..." />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
+                                        class="d-block w-100" alt="..." />
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#moviesCarousel"
+                                data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#moviesCarousel"
+                                data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div class="row my-5">
+                    <div class="col-6">
+                        <div id="carouselExampleIndicators" class="carousel slide">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                                    class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                                    aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                                    aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
+                                        class="d-block w-100" alt="..." />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
+                                        class="d-block w-100" alt="..." />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
+                                        class="d-block w-100" alt="..." />
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#moviesCarousel"
+                                data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#moviesCarousel"
+                                data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="col-6">
+                        <h2>Cosa visitare...</h2>
+                        <h5>...Malvito!</h5>
+                        <p>
+                            Malvito sorge sulla cima di un poggio alla destra dell' Esaro ed
+                            offre una veduta paesaggistica singolare. Si ritiene sia uno dei
+                            pochi centri calabresi di origine Longobarda, sede di Gastaldato.
+                            Dalla documentazione esistente si può stabilire che Malvito ebbe
+                            origine tra la fine del VII secolo e gli inizi del successivo,
+                            questo grazie al cronista longobardo Paolo Diacono che indica il
+                            centro dell' Esaro come una delle più importanti città dell'Italia
+                            meridionale. Durante il pontificato di Benedetto VII (anno 983),
+                            la chiesa di Malvito fu elevata a diocesi fino a quando, nel 1160,
+                            fu accorpata a quella di San Marco Argentano. Nel 1057 subì
+                            conquista Normanna di Roberto d'Altavilla chiamato il Guiscardo
+                            (furbo). Il primo feudatario fu il conte Roberto detto Scalone,
+                            terzogenito del guiscardo e della principessa Longobarda
+                            Sikelgaita.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
     </main>
 </template>
 
 <script>
-    export default {
-        name: 'MainComponent',
-    }
+export default {
+    name: "MainComponent",
+};
 </script>
 
 <style lang="scss" scoped>
-    main{
-        margin-top: 80px;
-    }
+main {
+    margin-top: 80px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import './assets/style/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 createApp(App).mount('#app')
 
