0ca7d832f6dbeb5bedfe5d0e6c1a74064d527fd1
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-06-28 19:51:46 +0200

    add img

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -26,101 +26,55 @@
                     </div>
 
                     <div class="col-6 ">
-                        <div id="carouselExampleIndicators" class="carousel slide">
-                            <div class="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                                    class="active" aria-current="true" aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                                    aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                                    aria-label="Slide 3"></button>
-                            </div>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
-                                        class="d-block w-100" alt="..." />
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
-                                        class="d-block w-100" alt="..." />
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
-                                        class="d-block w-100" alt="..." />
+                        <div class="col-6">
+                        <div id="container">
+                            <div class="row my-5">
+                                <div class="col-6">
+                                    <div class="container">
+                                        <img class="image top" src="https://via.placeholder.com/300x300?text=Top+Image"
+                                            alt="Top Image">
+                                        <img class="image bottom"
+                                            src="https://via.placeholder.com/300x300?text=Bottom+Image"
+                                            alt="Bottom Image">
+                                    </div>
                                 </div>
                             </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#moviesCarousel"
-                                data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#moviesCarousel"
-                                data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
                         </div>
                     </div>
+
+                    </div>	
 
                 </div>
 
 
                 <div class="row my-5">
                     <div class="col-6">
-                        <div id="carouselExampleIndicators" class="carousel slide">
-                            <div class="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                                    class="active" aria-current="true" aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                                    aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                                    aria-label="Slide 3"></button>
-                            </div>
-                            <div class="carousel-inner">
-                                <div class="carousel-item ">
-                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
-                                        class="d-block w-100" alt="..." />
-                                </div>
-                                <div class="carousel-item active">
-                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
-                                        class="d-block w-100" alt="..." />
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="https://149345965.v2.pressablecdn.com/wp-content/uploads/img-hotels-IADGV_006-Dusk-Exterior-home.jpg"
-                                        class="d-block w-100" alt="..." />
+                        <div id="container">
+                            <div class="row my-5">
+                                <div class="col-6">
+                                    <div class="container">
+                                        <img class="image top2" src="https://via.placeholder.com/300x300?text=Top+Image"
+                                            alt="Top Image">
+                                        <img class="image bottom2"
+                                            src="https://via.placeholder.com/300x300?text=Bottom+Image"
+                                            alt="Bottom Image">
+                                    </div>
                                 </div>
                             </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#moviesCarousel"
-                                data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#moviesCarousel"
-                                data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
                         </div>
                     </div>
 
                     <div class="col-6">
-                        <h2>Cosa visitare...</h2>
-                        <h5>...Malvito!</h5>
+                        <h2>il nostro B&B</h2>
                         <p>
-                            Malvito sorge sulla cima di un poggio alla destra dell' Esaro ed
-                            offre una veduta paesaggistica singolare. Si ritiene sia uno dei
-                            pochi centri calabresi di origine Longobarda, sede di Gastaldato.
-                            Dalla documentazione esistente si può stabilire che Malvito ebbe
-                            origine tra la fine del VII secolo e gli inizi del successivo,
-                            questo grazie al cronista longobardo Paolo Diacono che indica il
-                            centro dell' Esaro come una delle più importanti città dell'Italia
-                            meridionale. Durante il pontificato di Benedetto VII (anno 983),
-                            la chiesa di Malvito fu elevata a diocesi fino a quando, nel 1160,
-                            fu accorpata a quella di San Marco Argentano. Nel 1057 subì
-                            conquista Normanna di Roberto d'Altavilla chiamato il Guiscardo
-                            (furbo). Il primo feudatario fu il conte Roberto detto Scalone,
-                            terzogenito del guiscardo e della principessa Longobarda
-                            Sikelgaita.
+                            "Antica casina" si trova a pochi Km dal centro di Malvito , E' il posto ideale per ritrovare
+                            il vero contatto con la natura, lontano dalla confusione.Luogo ideale al relax e alle tante
+                            proposte per il tempo libero che la bella Calabria può offrire.
+                            Situato in una straordinaria posizione panoramica permette di godere di tramonti
+                            indimenticabili.
+                            Gli ospiti potranno soggiornare in un edificio camere situate al secondo piano,
+                            ristrutturato di recente con tutti i comfort.
+                            Il Bed and Breakfast "Antica casina" è dotato di ampio parcheggio privato.
                         </p>
                     </div>
                 </div>
@@ -128,12 +82,12 @@
 
             <div class="row my-5 text-center">
                 <h1 class="mb-5">Scopri i fantastici Servizi</h1>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-4 mb-4" v-for="(card, index) in cards" :key="index">
-                <div class="card">
-                    <img :src="card.image" class="card-img-top" alt="...">
+                <div class="col-12 col-sm-6 col-md-4 col-lg-4 mb-4" v-for="(card, index) in cards" :key="index">
+                    <div class="card">
+                        <img :src="card.image" class="card-img-top" :alt="card.title">
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     </main>
 </template>
@@ -144,37 +98,37 @@ export default {
     data() {
         return {
             cards: [
-            {
-                        title: 'Card title 1',
-                        description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                        image: 'https://via.placeholder.com/150'
-                    },
-                    {
-                        title: 'Card title 2',
-                        description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                        image: 'https://via.placeholder.com/150'
-                    },
-                    {
-                        title: 'Card title 3',
-                        description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                        image: 'https://via.placeholder.com/150'
-                    },
-                    {
-                        title: 'Card title 4',
-                        description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                        image: 'https://via.placeholder.com/150'
-                    },
-                    {
-                        title: 'Card title 4',
-                        description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                        image: 'https://via.placeholder.com/150'
-                    },
-                    {
-                        title: 'Card title 4',
-                        description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                        image: 'https://via.placeholder.com/150'
-                    }
-                ]
+                {
+                    title: 'Card title 1',
+                    description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+                    image: 'https://via.placeholder.com/100'
+                },
+                {
+                    title: 'Card title 2',
+                    description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+                    image: 'https://via.placeholder.com/100'
+                },
+                {
+                    title: 'Card title 3',
+                    description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+                    image: 'https://via.placeholder.com/100'
+                },
+                {
+                    title: 'Card title 4',
+                    description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+                    image: 'https://via.placeholder.com/100'
+                },
+                {
+                    title: 'Card title 4',
+                    description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+                    image: 'https://via.placeholder.com/100'
+                },
+                {
+                    title: 'Card title 4',
+                    description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+                    image: 'https://via.placeholder.com/100'
+                }
+            ]
         };
     }
 };
@@ -193,6 +147,53 @@ main {
             height: 100%;
             object-fit: cover;
         }
+    }
+
+    #container {
+        position: relative;
+        width: 300px;
+        /* Imposta la larghezza desiderata per il container */
+        height: 300px;
+        /* Imposta l'altezza desiderata per il container */
+    }
+
+    .image {
+        position: absolute;
+        width: 300px;
+        height: 200px;
+        object-fit: cover;
+        /* Per mantenere le proporzioni dell'immagine */
+    }
+
+    .image.top {
+        top: 0;
+        left: 120px;
+        /* Sposta leggermente a sinistra */
+        z-index: 0;
+        /* Posto sopra l'altra immagine */
+    }
+
+    .image.bottom {
+        top: 90px;
+        /* Sposta leggermente più in basso */
+        left: 0;
+        z-index: 1;
+        /* Posto sotto l'altra immagine */
+    }
+    .image.top2 {
+        top: 90px;
+        left: 120px;
+        /* Sposta leggermente a sinistra */
+        z-index: 1;
+        /* Posto sopra l'altra immagine */
+    }
+
+    .image.bottom2{
+        top: 0;
+        /* Sposta leggermente più in basso */
+        left: 0;
+        z-index: 0;
+        /* Posto sotto l'altra immagine */
     }
 }
 </style>
